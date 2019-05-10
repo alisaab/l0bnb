87908a00b8e99a95ef72b475850cf9c5b472b652
alisaab <alikassemsaab@gmail.com>
2019-05-10 00:13:26 -0400

    the norm of columns as an input, initial active active set, and conic formulation

--- l0bnb/relaxation.py
+++ l0bnb/relaxation.py
@@ -9,10 +9,10 @@ def _calculate_cost(beta, r, l0, l2, golden_ratio, m, zlb, zub):
     z = abs(beta) / m
     z[z > 0] = np.maximum(z[z > 0], beta[z > 0] ** 2 / s[z > 0])
     z = np.minimum(np.maximum(zlb, z), zub)
-    return np.dot(r, r) / 2 + l0 * sum(z) + l2 * sum(s), z
+    return np.dot(r, r) / 2 + l0 * sum(z[z > 0]) + l2 * sum(s[s > 0]), z
 
 
-def _coordinate_descent_loop(x, beta, index_map, l2, golden_ratio, threshold, m, zlb, zub, support, r):
+def _coordinate_descent_loop(x, beta, index_map, l2, golden_ratio, threshold, m, xi_xi, zlb, zub, support, r):
     zub_active_is_zero = np.where(zub == 0)[0]
     zlb_active_normal = np.where(np.logical_and(zlb == 0, zub > 0))[0]
     zlb_active_is_one = np.where(zlb > 0)[0]
@@ -26,23 +26,22 @@ def _coordinate_descent_loop(x, beta, index_map, l2, golden_ratio, threshold, m,
 
     for i in zub_active_is_zero:
         r = r + dot_product(beta[i], x[:, i])
-        set_discard(index_map[i])
+        # set_discard(index_map[i])
         beta[i] = 0
 
     for i in zlb_active_normal:
         x_i = x[:, i]
-        xi_xi = np.dot(x_i, x_i)
         r = r + dot_product(beta[i], x_i)
         ri_xi = dot_product(r, x_i)
         abs_ri_xi = abs(ri_xi)
         if abs_ri_xi <= threshold:
-            set_discard(index_map[i])
+            # set_discard(index_map[i])
             beta[i] = 0
         else:
             if index_map[i] not in support:
                 set_add(index_map[i])
-            criteria = (abs_ri_xi - threshold) / xi_xi
-            criteria = update_criteria(criteria, abs_ri_xi, xi_xi)
+            criteria = (abs_ri_xi - threshold) / xi_xi[i]
+            criteria = update_criteria(criteria, abs_ri_xi, xi_xi[i])
             beta[i] = (criteria if criteria < m else m) * np.sign(ri_xi)
             r = r - dot_product(beta[i], x_i)
 
@@ -50,7 +49,7 @@ def _coordinate_descent_loop(x, beta, index_map, l2, golden_ratio, threshold, m,
         x_i = x[:, i]
         r = r + dot_product(beta[i], x_i)
         ri_xi = dot_product(r, x_i)
-        criteria = abs(ri_xi) / (2 * l2 + np.dot(x_i, x_i))
+        criteria = abs(ri_xi) / (2 * l2 + xi_xi[i])
         if index_map[i] not in support:
             set_add(index_map[i])
         beta[i] = (criteria if criteria < m else m) * np.sign(ri_xi)
@@ -59,7 +58,7 @@ def _coordinate_descent_loop(x, beta, index_map, l2, golden_ratio, threshold, m,
     return beta, r
 
 
-def coordinate_descent(x, beta, cost, l0, l2, golden_ratio, threshold, m, zlb, zub, support, r, reltol):
+def coordinate_descent(x, beta, cost, l0, l2, golden_ratio, threshold, m, xi_xi, zlb, zub, support, r, reltol):
     tol = 1
     while tol > reltol:
         old_cost = cost
@@ -67,42 +66,54 @@ def coordinate_descent(x, beta, cost, l0, l2, golden_ratio, threshold, m, zlb, z
         zlb_active = zlb[active_set]
         zub_active = zub[active_set]
         beta_active = beta[active_set]
+        xi_xi_active = xi_xi[active_set]
         x_active = x[:, active_set]
         beta_active, r = _coordinate_descent_loop(x_active, beta_active, active_set, l2, golden_ratio, threshold, m,
-                                                  zlb_active, zub_active, support, r)
+                                                  xi_xi_active, zlb_active, zub_active, support, r)
         beta[active_set] = beta_active
-        cost, _ = _calculate_cost(beta, r, l0, l2, golden_ratio, m, zlb, zub)
+        cost, _ = _calculate_cost(beta[active_set], r, l0, l2, golden_ratio, m, zlb[active_set], zub[active_set])
         tol = abs(1 - old_cost / cost)
     return beta, cost, r
 
 
-def initial_active_set(x, beta, l2, golden_ratio, threshold, m, zlb, zub, support, r):
+def initial_active_set(y, x, beta, l2, golden_ratio, threshold, m, xi_xi, zlb, zub, support, r):
     num_of_similar_supports = 0
-    active_set = list(range(len(beta)))
+    correlations = np.matmul(y, x)/xi_xi
+    partition = np.argpartition(-correlations, min(2000, len(beta)))
+    active_set = list(partition[0:min(2000, len(beta))])
+    zlb_active = zlb[active_set]
+    zub_active = zub[active_set]
+    beta_active = beta[active_set]
+    xi_xi_active = xi_xi[active_set]
+    x_active = x[:, active_set]
     while num_of_similar_supports < 3:
         old_support = copy.deepcopy(support)
-        beta, r = _coordinate_descent_loop(x, beta, active_set, l2, golden_ratio, threshold, m, zlb, zub, support, r)
+        beta_active, r = _coordinate_descent_loop(x_active, beta_active, active_set, l2, golden_ratio, threshold, m,
+                                                  xi_xi_active, zlb_active, zub_active, support, r)
         if old_support == support:
             num_of_similar_supports += 1
         else:
             num_of_similar_supports = 0
+        kes, _ = _calculate_cost(beta_active, r, 70, l2, golden_ratio, m, zlb_active, zub_active)
+        print(kes, num_of_similar_supports)
+    beta[active_set] = beta_active
     return support, r
 
 
-def relaxation_solve(x, y, l0, l2, m, zlb, zub, beta, r, reltol=1e-12):
+def relaxation_solve(x, y, l0, l2, m, xi_xi, zlb, zub, beta, r, reltol=1e-12):
     p = x.shape[1]
     golden_ratio = np.sqrt(l0 / l2) if l2 != 0 else np.Inf
     threshold = 2 * np.sqrt(l0 * l2) if golden_ratio <= m else l0 / m + l2 * m
     if beta is None:
         beta = np.zeros(p)
         r = y - np.matmul(x, beta)
-        support, r = initial_active_set(x, beta, l2, golden_ratio, threshold, m, zlb, zub, set(), r)
+        support, r = initial_active_set(y, x, beta, l2, golden_ratio, threshold, m, xi_xi, zlb, zub, set(), r)
     else:
         support = set(abs(beta).nonzero()[0])
     cost, _ = _calculate_cost(beta, r, l0, l2, golden_ratio, m, zlb, zub)
     while True:
-        beta, cost, r = coordinate_descent(x, beta, cost, l0, l2, golden_ratio, threshold, m, zlb, zub, support, r,
-                                           reltol)
+        beta, cost, r = coordinate_descent(x, beta, cost, l0, l2, golden_ratio, threshold, m, xi_xi, zlb, zub, support,
+                                           r, reltol)
         above_threshold = np.where(zub * abs(np.matmul(r, x)) - threshold > 0)[0]
         outliers = [i for i in above_threshold if i not in support]
         if not outliers:
