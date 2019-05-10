8023fae1fa862a991c54a0dee5e298dcdb015d15
alisaab <alikassemsaab@gmail.com>
2019-05-10 00:14:03 -0400

    update

--- quick_run.py
+++ quick_run.py
@@ -10,44 +10,36 @@ from l0bnb.bnb import bnb
 from profiler import profile
 from generate_data import GenData as gen_data
 
-n = 10000
-p = 10000
+n = 1000
+p = 100000
 rho = 0.5
-supp_size = 5
-snr = 10
+supp_size = 10
+snr = 20
 m = 1.2
-l0 = 100
-l2 = 50
+l0 = 200
+l2 = 20
 using_upper_bound = True
 inttol = 1e-4
-gaptol = 0.01 #1e-5
-reltol = 1e-4
-branching = 'maxfrac'
+gaptol = 5e-2
+reltol = 1e-3
+branching = 'strong'
 l1solver = 'l1cd'
-mu = 0.9
-
+mu = 0.95
+bnb_algorithm = 'BFS'
+corr = 'CLarge'
 # generate data
-x, y, features, covariance = gen_data('CLarge', rho, n, p, supp_size, snr) # CLarge
+x, y, features, covariance = gen_data(corr, rho, n, p, supp_size, snr)  # CLarge
 print("Generated data!")
-#print(np.corrcoef(x,rowvar=False)) # to estimate corr matrix from data
+# print(np.corrcoef(x,rowvar=False)) # to estimate corr matrix from data
 
-# from l0bnb.node import Node
 # zlb = np.zeros(p)
-# zlb[3] = 1
 # zub = np.ones(x.shape[1])
-# try_node_r = Node(0, None, zlb, zub)
-# try_node_r.compute_lower_bound(x, y, l0, l2, m, reltol, 'l1cd', None)
-# print(try_node_r.lower_bound, '\n', try_node_r.lower_bound_solution, '\n', try_node_r.lower_bound_z)
-# print('---------------------------')
-# try_node_g = Node(0, None, zlb, zub)
-# try_node_g.compute_lower_bound(x, y, l0, l2, m, reltol, 'gurobi', None)
-# print(try_node_g.lower_bound, '\n', try_node_g.lower_bound_solution, '\n', try_node_g.lower_bound_z)
+# zlb[[833, 6777, 9085, 9842]] = 1
+# zub[[1006, 3585]] = 0
 # from l0bnb.gurobi_solve import l0gurobi
 # from l0bnb.relaxation import relaxation_solve
-# a,b,c = l0gurobi(x, y, l0, l2, m, zlb, zub, False)
-# print('----------')
+# a, b, c = l0gurobi(x, y, l0, l2, m, zlb, zub, True)
 # a1, _, b1, c1 = relaxation_solve(x, y, l0, l2, m, zlb, zub, None, None)
-# print(a,b,c)
 
 # for i in range(x.shape[1]):
 #     x[:, i] /= np.linalg.norm(x[:, i])
@@ -68,28 +60,29 @@ else:
     upper_bound_solution[support] = res.x
 
 # Solve the best subset problem
-# st = time()
-# solB, objB, edges, lower_bound = bnb(x, y, l0, l2, m, upperbound=upper_bound, uppersol=upper_bound_solution,
-#                                      inttol=inttol, gaptol=gaptol, reltol=reltol, branching=branching, mu=mu,
-#                                      l1solver=l1solver)
-# tot_time = time() - st
-# G = nx.Graph()
-# G.add_edges_from(edges)
-# pos = nx.drawing.nx_agraph.graphviz_layout(G, prog='dot')
-# nx.draw(G, pos)
-# to_write = ('n = ' + str(n) + '\np = ' + str(p) + '\nsnr = ' + str(snr) + '\nsupport size = ' + str(supp_size)
-#             + '\nlambda = ' + str(l0) + '\nm = ' + str(m) + '\ntotal time = ' + str(round(tot_time, 3)) +
-#             '\nupper bound = ' + str(using_upper_bound) + '\nbranching = ' + branching + '\nl1solver = ' + l1solver
-#             + '\ngap = ' + str(gaptol) + '\nnumber of nodes = ' + str(len(edges)) + '\nlevels = ' + str(
-#             max(lower_bound)))
-# plt.text(20, 200, to_write)
-# plt.savefig('11.eps', format='eps', dpi=1000)
+st = time()
+solB, objB, edges, lower_bound, best_gap = bnb(x, y, l0, l2, m, upperbound=upper_bound, uppersol=upper_bound_solution,
+                                               inttol=inttol, gaptol=gaptol, reltol=reltol, branching=branching, mu=mu,
+                                               l1solver=l1solver, bnb_algorithm=bnb_algorithm)
+tot_time = time() - st
+G = nx.Graph()
+G.add_edges_from(edges)
+pos = nx.drawing.nx_agraph.graphviz_layout(G, prog='dot')
+nx.draw(G, pos)
+to_write = ('n = ' + str(n) + '\np = ' + str(p) + '\nsnr = ' + str(snr) + '\nsupport size = ' + str(supp_size) +
+            '\nl0 = ' + str(l0) + '\nl2 = ' + str(l2) + '\nm = ' + str(m) +
+            '\ntotal time = ' + str(round(tot_time, 3)) + '\nupper bound = ' + str(using_upper_bound) +
+            '\nbranching = ' + branching + '\nl1solver = ' + l1solver + '\ngap = ' + str(best_gap) +
+            '\nnumber of nodes = ' + str(len(edges)) + '\nlevels = ' + str(max(lower_bound)) +
+            '\nalgorithm = ' + str(bnb_algorithm) + '\ncorr = ' + corr + '\nrho = ' + str(rho))
+plt.text(20, 0, to_write)
+plt.savefig('9-20-s.eps', format='eps', dpi=1000)
 
 # upper_bound = 0.5*np.dot(y-np.matmul(X, features), y-np.matmul(X, features)) + 10*sum(features != 0)
 # print(upper_bound)
-print(profile(bnb, x, y, l0, l2, m, upperbound=upper_bound, uppersol=upper_bound_solution,
-              inttol=inttol, gaptol=gaptol, reltol=reltol, branching=branching, mu=mu,
-              l1solver=l1solver))
+# print(profile(bnb, x, y, l0, l2, m, upperbound=upper_bound, uppersol=upper_bound_solution,
+#               inttol=inttol, gaptol=gaptol, reltol=reltol, branching=branching, mu=mu,
+#               l1solver=l1solver))
 # beta, z, cost = relaxation_solve(X, y, 120, 100/120, lb, ub, kes)
 # st = time()
 # solG, zG, objG = best_subset_selection(X, y, 120, 10, {i: 0 for i in range(p)}, {i: 1 for i in range(p)}, relaxed=False)
