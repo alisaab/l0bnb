5897f05020e21afc022c9d01e001610ed17ce866
alisaab <alikassemsaab@gmail.com>
2019-11-17 23:17:35 -0500

    update node class

--- l0bnb/node.py
+++ l0bnb/node.py
@@ -1,4 +1,5 @@
-import copy
+from copy import deepcopy
+
 import numpy as np
 from scipy import optimize as sci_opt
 
@@ -7,46 +8,111 @@ from .gurobi_solve import l0gurobi
 
 
 class Node:
-    def __init__(self, node_num, parent, new_zlb, new_zub):
-        self.node_num = node_num
+    def __init__(self, parent, zlb, zub, **kwargs):
+        """
+        Initialize a Node
+
+        Parameters
+        ----------
+        parent: Node or None
+            the parent Node
+        zlb: np.array
+            p x 1 array representing the lower bound of the integer variables z
+        zub: np.array
+            p x 1 array representing the upper bound of the integer variables z
+
+        Other Parameters
+        ----------------
+        x: np.array
+            The data matrix (n x p). If not specified the data will be inherited
+            from the parent node
+        y: np.array
+            The data vector (n x 1). If not specified the data will be inherited
+            from the parent node
+        xi_xi: np.array
+            The norm of each column in x (p x 1). If not specified the data will
+            be inherited from the parent node
+        l0: float
+            The zeroth norm coefficient. If not specified the data will
+            be inherited from the parent node
+        l2: float
+            The second norm coefficient. If not specified the data will
+            be inherited from the parent node
+        m: float
+            The bound for the features (\beta). If not specified the data will
+            be inherited from the parent node
+        """
         self.parent = parent
-        self.zlb = new_zlb
-        self.zub = new_zub
+        self.x = kwargs.get('x', parent.x if parent else None)
+        self.y = kwargs.get('y', parent.y if parent else None)
+        self.xi_xi = kwargs.get('xi_xi', parent.xi_xi if parent else None)
+        self.l0 = kwargs.get('l0', parent.l0 if parent else None)
+        self.l2 = kwargs.get('l2', parent.l2 if parent else None)
+        self.m = kwargs.get('m', parent.m if parent else None)
+        self.initial_guess = deepcopy(self.parent.lower_bound_solution) \
+            if parent else None
+        self.r = deepcopy(self.parent.r) if parent else None
 
+        self.p = self.x.shape[1]
         self.level = parent.level + 1 if parent else 0
-        self.initial_guess = copy.deepcopy(self.parent.lower_bound_solution) if parent else None
-        self.r = copy.deepcopy(self.parent.r) if parent else None
 
-        self.upper_bound = None
+        self.zlb = zlb
+        self.zub = zub
+
+        self.upper_bound_value = None
         self.upper_bound_solution = None
+        self.lower_bound_value = None
         self.lower_bound_solution = None
         self.lower_bound_z = None
-        self.lower_bound = None
+        self.support = None
 
-    def compute_lower_bound(self, x, y, l0, l2, m, xi_xi, reltol, solver, initial_guess):
+    def lower_solve(self, solver, reltol, inttol=None):
         if solver == 'l1cd':
-            self.lower_bound_solution, self.r, self.lower_bound_z, self.lower_bound = \
-                relaxation_solve(x, y, l0, l2, m, xi_xi, self.zlb, self.zub, initial_guess, self.r, reltol)
+            self.lower_bound_value, self.lower_bound_solution, \
+                self.lower_bound_z, self.r, self.support = \
+                relaxation_solve(self.x, self.y, self.l0, self.l2, self.m,
+                                 self.xi_xi, self.zlb, self.zub,
+                                 self.initial_guess, self.r, reltol)
+            self.support = list(self.support)
         elif solver == 'gurobi':
-            self.lower_bound_solution, self.lower_bound_z, self.lower_bound = \
-                l0gurobi(x, y, l0, l2, m, self.zlb, self.zub, relaxed=True)
-        return self.lower_bound
+            if not inttol:
+                raise ValueError('inttol should be specified to use gurobi')
+            self.lower_bound_solution, self.lower_bound_z, \
+                self.lower_bound_value = \
+                l0gurobi(self.x, self.y, self.l0, self.l2, self.m, self.zlb,
+                         self.zub, relaxed=True)
+            self.support = \
+                list(np.where(abs(self.lower_bound_solution) > inttol)[0])
+        return self.lower_bound_value
 
-    def compute_upper_bound(self, x, y, l0, l2, m, int_tol):
-        support = list(np.where(abs(self.lower_bound_solution) > int_tol)[0])
-        x_support = x[:, support]
-        x_ridge = np.sqrt(2 * l2) * np.identity(len(support))
+    def upper_solve(self):
+        self.support = \
+            list(np.where(abs(self.lower_bound_solution) > 1e-4)[0])
+        x_support = self.x[:, self.support]
+        x_ridge = np.sqrt(2 * self.l2) * np.identity(len(self.support))
         x_upper = np.concatenate((x_support, x_ridge), axis=0)
-        y_upper = np.concatenate((y, np.zeros(len(support))), axis=0)
-        res = sci_opt.lsq_linear(x_upper, y_upper, (-m, m))  # account for intercept later
-        self.upper_bound = res.cost + l0 * len(support)
-        self.upper_bound_solution = np.zeros(x.shape[1])
-        self.upper_bound_solution[support] = res.x
-        return self.upper_bound
+        y_upper = np.concatenate((self.y, np.zeros(len(self.support))), axis=0)
+        # account for intercept later
+        res = sci_opt.lsq_linear(x_upper, y_upper, (-self.m, self.m))
+        self.upper_bound_value = res.cost + self.l0 * len(self.support)
+        self.upper_bound_solution = np.zeros(self.p)
+        self.upper_bound_solution[self.support] = res.x
+        return self.upper_bound_value
 
     def strong_branch_solve(self, x, l0, l2, m, xi_xi, support):
         golden_ratio = np.sqrt(l0 / l2) if l2 != 0 else np.Inf
-        threshold = 2 * np.sqrt(l0 * l2) if golden_ratio <= m else l0 / m + l2 * m
-        _, cost, _ = coordinate_descent(x, self.initial_guess, self.parent.lower_bound, l0, l2, golden_ratio,
-                                        threshold, m, xi_xi, self.zlb, self.zub, support, self.r, 0.1)
+        threshold = 2 * np.sqrt(l0 * l2) if golden_ratio <= m \
+            else l0 / m + l2 * m
+        _, cost, _ = \
+            coordinate_descent(x, self.initial_guess,
+                               self.parent.lower_bound_value,
+                               l0, l2, golden_ratio, threshold, m, xi_xi,
+                               self.zlb, self.zub, support, self.r, 0.9)
         return cost
+
+    def __str__(self):
+        return f'level: {self.level}, lower cost: {self.lower_bound_value}, ' \
+            f'upper cost: {self.upper_bound_value}'
+
+    def __repr__(self):
+        return self.__str__()
