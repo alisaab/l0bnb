2abfee11116433742452cacd3293f2e2644b4308
Ali Saab <alikassemsaab@gmail.com>
2020-04-09 01:56:56 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
-# from setuptools import setup
-#
-# setup(
-#    name='foo',
-#    version='1.0',
-#    description='A useful module',
-#    author='Man Foo',
-#    author_email='foomail@foo.com',
-#    packages=['foo'],
-#    install_requires=[],
-# )
+from setuptools import setup, find_packages
+
+LICENSE = """MIT"""
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name="l0bnb",
+    description="least squares regression with l0l2 regularization",
+    author="Ali Saab",
+    author_email="alikassemsaab@gmail.com",
+    install_requires=["numpy", "scipy", "numba", "matplotlib"],
+    version="0.0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    license=LICENSE,
+    long_description=readme(),
+)
