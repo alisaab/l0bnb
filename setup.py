ac84d3da749ab4bfa8bf1163c42e20f79c8a14de
alisaab <alikassemsaab@gmail.com>
2020-04-14 21:06:20 -0400

    packaging

--- setup.py
+++ setup.py
@@ -11,8 +11,10 @@ def readme():
 setup(
     name="l0bnb",
     description="least squares regression with l0l2 regularization",
-    author="Ali Saab",
+    author="Ali Saab, Hussein Hazimeh, Rahul Mazumder",
     author_email="alikassemsaab@gmail.com",
+    url='https://github.com/alisaab/l0bnb',
+    download_url="https://github.com/alisaab/l0bnb/archive/0.0.1.tar.gz",
     install_requires=["numpy >= 1.18.2", "scipy >= 1.4.1", "numba >= 0.48.0",
                       "matplotlib >= 3.2.1"],
     version="0.0.1",
