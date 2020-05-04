a6320de584d75c596a986fd12a5bd413ab8200cf
alisaab <alikassemsaab@gmail.com>
2020-05-04 05:27:44 -0400

    filter warnings

--- l0bnb/relaxation/__init__.py
+++ l0bnb/relaxation/__init__.py
@@ -3,10 +3,10 @@ import warnings
 from numba.errors import NumbaDeprecationWarning, \
     NumbaPendingDeprecationWarning, NumbaPerformanceWarning
 
-from .core import solve as cd_solve
-from .mosek import l0mosek
-from .gurobi import l0gurobi
-
 warnings.simplefilter('ignore', category=NumbaDeprecationWarning)
 warnings.simplefilter('ignore', category=NumbaPendingDeprecationWarning)
 warnings.simplefilter('ignore', category=NumbaPerformanceWarning)
+
+from .core import solve as cd_solve
+from .mosek import l0mosek
+from .gurobi import l0gurobi
